00443049a69d089a5a4d06675c0cf3c850141c66
PinkyOne <alexsanr56@gmail.com>
2014-10-28 20:31:57 +0400

    asd

---
+++ labCG3Turn3/Brezenham.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labCG3Turn3
+{
+    using System.Drawing;
+
+    class Brezenham
+    {
+        public static void BresenhamCircle(Graphics g, Color clr, int _x, int _y, int radius)
+        {
+            int x = 0, y = radius, gap = 0, delta = (2 - 2 * radius);
+            while (y >= 0)
+            {
+                PutPixel(g, clr, _x + x, _y + y, 255);
+                PutPixel(g, clr, _x + x, _y - y, 255);
+                PutPixel(g, clr, _x - x, _y - y, 255);
+                PutPixel(g, clr, _x - x, _y + y, 255);
+
+                gap = 2 * (delta + y) - 1;
+
+                if (delta < 0 && gap <= 0)
+                {
+                    x++;
+
+                    delta += 2 * x + 1;
+
+                    continue;
+                }
+                if (delta > 0 && gap > 0)
+                {
+                    y--;
+
+                    delta -= 2 * y + 1;
+
+                    continue;
+                }
+                x++;
+                delta += 2 * (x - y);
+                y--;
+            }
+        }
+
+        // Метод, устанавливающий пиксел на форме с заданными цветом и прозрачностью
+        private static void PutPixel(Graphics g, Color col, int x, int y, int alpha)
+        {
+            g.FillRectangle(new SolidBrush(Color.FromArgb(alpha, col)), x, y, 1, 1);
+        }
+
+        private static int IPart(float x)
+        {
+            return (int)x;
+        }
+
+        private static float FPart(float x)
+        {
+            while (x >= 0)
+                x--;
+            x++;
+            return x;
+        }
+    }
+}
---
+++ labCG3Turn3/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LabCG3Turn3
+{
+    using System.Threading;
+
+    using labCG3Turn3;
+
+    public partial class Form1 : Form
+    {
+        private readonly BezierCurve bezier;
+        private readonly Marker[] markers = new Marker[4];
+
+        public Form1()
+        {
+            InitializeComponent();
+            markers[0] = new Marker(150, 200);
+            markers[1] = new Marker(300, 350);
+            markers[2] = new Marker(300, 50);
+            markers[3] = new Marker(450, 200);
+            for (int index = 0; index < markers.Length; index++)
+            {
+                Marker marker = markers[index];
+                int i = index;
+                marker.OnDrag += f =>
+                {
+                    bezier[i] = f;
+                    pictureBox1.Invalidate();
+                };
+                marker.OnMouseDown += f => { Cursor = Cursors.Hand; };
+            }
+            bezier = new BezierCurve(markers.Select(m => m.Location).ToArray());
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+            Pen pen = new Pen(Color.Gray, 1f);
+            e.Graphics.DrawLines(pen, markers.Select(m => m.Location).ToArray());
+            foreach (Marker marker in markers)
+            {
+                marker.Draw(e.Graphics);
+            }
+            bezier.Draw(e.Graphics);
+
+            Brezenham.BresenhamCircle(e.Graphics, Color.Black, 300, 200, 150);
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                foreach (Marker marker in markers)
+                {
+                    marker.MouseMove(e);
+                    Thread.Sleep(0);
+                }
+            }
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            foreach (Marker marker in markers)
+            {
+                marker.MouseDown(e);
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            foreach (Marker marker in markers)
+            {
+                marker.MouseUp();
+            }
+            Cursor = Cursors.Arrow;
+        }
+    }
+}
